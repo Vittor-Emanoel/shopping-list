054a8a7ae9083aafe6d507acbfe362ec56dd53f2
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-01-19 17:38:01 -0300

    feat: create header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Select, SelectContent, SelectTrigger } from "@radix-ui/react-select";
+import { Input } from "./components/ui/input";
+import { SelectItem } from "./components/ui/select";
+import cover from "./imgs/cover.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
+    <div className="w-full text-white relative">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={cover} alt="cover image" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="container absolute right-0 left-0 top-[76px]">
+        <header>
+          <h1 className="text-gray-100 font-bold text-2xl mb-8">
+            Lista de Compras
+          </h1>
 
-export default App
+          <form action="" className="flex bo">
+            <div className="flex flex-col gap-2">
+              <label className="text-xs text-base-gray_200 font-normal leading-3 tracking-tight">
+                Item
+              </label>
+              <Input
+                type="text"
+                className="bg-base-gray_500 border-base-gray_300 "
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="text-xs text-base-gray_200 font-normal leading-3 tracking-tight">
+                Quantidade
+              </label>
+              <div className="flex ">
+                <Input
+                  type="text"
+                  className="bg-base-gray_500 border-base-gray_300 "
+                />
+                <Select>
+                  <SelectTrigger className="w-[72px] h-10 p-3 bg-zinc-900 border-purple border-2 rounded-tr-md rounded-br-md  justify-start items-center gap-2 inline-flex"></SelectTrigger>
+                  <SelectContent
+                    position="popper"
+                    className=" text-center bg-base-gray_500 rounded border border-neutral-800 "
+                  >
+                    <SelectItem
+                      value="vavdd"
+                      className="border-b border-neutral-800 "
+                    >
+                      Uni .
+                    </SelectItem>
+                    <SelectItem
+                      value="vavdd"
+                      className="border-b border-neutral-800 "
+                    >
+                      L
+                    </SelectItem>
+                    <SelectItem value="vav">G</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </form>
+        </header>
+      </div>
+    </div>
+  );
+}
---
+++ front/tailwind.config.js
@@ -0,0 +1,105 @@
+/** @type {import('tailwindcss').Config} */
+export const darkMode = ["class"];
+export const content = [
+  "./pages/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}",
+];
+export const prefix = "";
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
+    },
+  },
+  extend: {
+    colors: {
+      purpleLight: "#A881E6",
+      purple: "#7450AC",
+      purpleDark: "#523480",
+      base: {
+        gray_100: "#FBF9FE",
+        gray_200: "#AFABB6",
+        gray_300: "#252529",
+        gray_400: "#17171A",
+        gray_500: "#111112",
+        gray_600: "#0C0C0D",
+      },
+      feedback: {
+        success_light: "#4E995E",
+        success: "#2F723D",
+      },
+      others: {
+        pink: "#DB5BBF",
+        pink_dark: "#251622",
+        orange: "#E07B67",
+        orange_dark: "#261A17",
+        yellow: "#BB9F3A",
+        yellow_dark: "#211E12",
+        green: "#8CAD51",
+        green_dark: "#1C2015",
+        blue: "#7B94CB",
+        blue_dark: "#1A1D23",
+      },
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      },
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
+      },
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
+      },
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+      accent: {
+        DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
+      },
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+      card: {
+        DEFAULT: "hsl(var(--card))",
+        foreground: "hsl(var(--card-foreground))",
+      },
+    },
+    fontFamily: {
+      base: ["'Inter'", "sans-serif"],
+    },
+    borderRadius: {
+      lg: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
+      sm: "calc(var(--radius) - 4px)",
+    },
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+  },
+};
+export const plugins = [import("tailwindcss-animate")];
